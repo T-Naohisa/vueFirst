05a08f7714b634986cb3207ae7a14fed600926ac
naohisa0820 <nao8.20y.peace@gmail.com>
2023-09-24 12:14:35 +0900

    eslintとpritterの設定と書き換え

--- vue-typescript-sample/.eslintrc.js
+++ vue-typescript-sample/.eslintrc.js
@@ -4,16 +4,32 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
   ],
+  plugins: ['@typescript-eslint'],
+  // Prerequisite `eslint-plugin-vue`, being extended, sets
+  // root property `parser` to `'vue-eslint-parser'`, which, for code parsing,
+  // in turn delegates to the parser, specified in `parserOptions.parser`:
+  // https://github.com/vuejs/eslint-plugin-vue#what-is-the-use-the-latest-vue-eslint-parser-error
   parserOptions: {
-    ecmaVersion: 2020,
+    parser: require.resolve('@typescript-eslint/parser'),
+    extraFileExtensions: ['.vue'],
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        // The core 'no-unused-vars' rules (in the eslint:recommeded ruleset)
+        // does not work with type definitions
+        'no-unused-vars': 'off',
+      },
+    },
+  ],
 };
