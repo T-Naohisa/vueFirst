1882f51e622952fda7f579049332a1d18f8af08d
naohisa0820 <nao8.20y.peace@gmail.com>
2023-10-09 16:32:05 +0900

    Todoリストの追加機能を追加

--- vue-typescript-sample/src/App.vue
+++ vue-typescript-sample/src/App.vue
@@ -8,32 +8,28 @@ export interface Todos {
 export default defineComponent({
   data() {
     return {
-      todos: [
-        // {
-        //   isDone: true,
-        //   text: 'Vueをマスターする',
-        // },
-        // {
-        //   isDone: false,
-        //   text: '牛乳を買う',
-        // },
-        // {
-        //   isDone: false,
-        //   text: '家賃を払う',
-        // },
-      ] as Array<Todos>,
+      todos: [] as Array<Todos>,
+      newTodo: '' as string,
       num: 0 as number,
       str: '' as string,
     };
   },
-  methods: {},
+  methods: {
+    addTodo() {
+      this.todos.push({
+        isDone: false,
+        text: this.newTodo,
+      });
+      this.newTodo = '';
+    },
+  },
 });
 </script>
 
 <template>
   <dev>
     <h1>My ToDo App</h1>
-    <input type="text" /><button>追加</button><button>完了済みを削除する</button>
+    <input type="text" v-model="newTodo" /><button @click="addTodo">追加</button><button>完了済みを削除する</button>
     <p v-if="todos.length === 0">ToDoがありません。</p>
     <ul v-else>
       <li v-for="(todo, index) in todos" :key="index">
