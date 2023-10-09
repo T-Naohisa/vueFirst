8408af4cfb8a6c4ada7b56478713b436bc777f62
naohisa0820 <nao8.20y.peace@gmail.com>
2023-10-09 16:14:08 +0900

    型定義の追加とTodoが0の時の処理を追加

--- vue-typescript-sample/src/App.vue
+++ vue-typescript-sample/src/App.vue
@@ -1,23 +1,29 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+export interface Todos {
+  isDone: boolean;
+  text: string;
+}
 
 export default defineComponent({
   data() {
     return {
       todos: [
-        {
-          isDone: true,
-          text: 'Vueをマスターする',
-        },
-        {
-          isDone: false,
-          text: '牛乳を買う',
-        },
-        {
-          isDone: false,
-          text: '家賃を払う',
-        },
-      ],
+        // {
+        //   isDone: true,
+        //   text: 'Vueをマスターする',
+        // },
+        // {
+        //   isDone: false,
+        //   text: '牛乳を買う',
+        // },
+        // {
+        //   isDone: false,
+        //   text: '家賃を払う',
+        // },
+      ] as Array<Todos>,
+      num: 0 as number,
+      str: '' as string,
     };
   },
   methods: {},
@@ -28,7 +34,8 @@ export default defineComponent({
   <dev>
     <h1>My ToDo App</h1>
     <input type="text" /><button>追加</button><button>完了済みを削除する</button>
-    <ul>
+    <p v-if="todos.length === 0">ToDoがありません。</p>
+    <ul v-else>
       <li v-for="(todo, index) in todos" :key="index">
         <input type="checkbox" /><span>{{ todo.text }}</span>
       </li>
