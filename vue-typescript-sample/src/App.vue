3f40e1e438036efd9a12fc5a7d93dcda17331481
naohisa0820 <nao8.20y.peace@gmail.com>
2023-10-14 13:55:34 +0900

    todo機能追加

--- vue-typescript-sample/src/App.vue
+++ vue-typescript-sample/src/App.vue
@@ -16,11 +16,19 @@ export default defineComponent({
   },
   methods: {
     addTodo() {
+      if (this.newTodo === '') {
+        alert('文字を入力してください。');
+        return;
+      }
       this.todos.push({
         isDone: false,
         text: this.newTodo,
       });
       this.newTodo = '';
+    },
+    clearDoneTodos() {
+      // チェックボックスにチェックのないもののみ抽出して新しいtodoリストを作成する
+      this.todos = this.todos.filter((todo) => !todo.isDone);
     },
   },
 });
@@ -29,11 +37,14 @@ export default defineComponent({
 <template>
   <dev>
     <h1>My ToDo App</h1>
-    <input type="text" v-model="newTodo" /><button @click="addTodo">追加</button><button>完了済みを削除する</button>
+    <input type="text" v-model="newTodo" /><button @click="addTodo">追加</button
+    ><button @click="clearDoneTodos">完了済みを削除する</button>
     <p v-if="todos.length === 0">ToDoがありません。</p>
     <ul v-else>
       <li v-for="(todo, index) in todos" :key="index">
-        <input type="checkbox" /><span>{{ todo.text }}</span>
+        <input type="checkbox" v-model="todo.isDone" /><span :class="{ 'todo-done': todo.isDone }">{{
+          todo.text
+        }}</span>
       </li>
     </ul>
   </dev>
