82fd315c72a79f7d2266d41f6b88f7cb9df396f1
naohisa0820 <nao8.20y.peace@gmail.com>
2023-10-22 11:32:46 +0900

    slotを利用したコンポーネント化

---
+++ vue-typescript-sample/src/components/basics/MyButton.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<template>
+  <button class="my-btn">
+    <slot />
+  </button>
+</template>
+<style>
+.my-btn {
+  color: #fff;
+  background-color: #333;
+  margin-left: 0.5em;
+  border-radius: 0.5em;
+}
+</style>
